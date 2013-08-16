4ab4f9d7d38411e612da44b86a837d31250ffeb3
Denis Biryukov <denis.birukov@gmail.com>
2013-08-16 23:18:48 +0400

    Add perf test

---
+++ tools/perf_test/perftest.cpp
@@ -0,0 +1,16 @@
+#include "cppprofiler.h"
+#include <iostream>
+#include <unistd.h>
+
+int main()
+{
+	CPPProfiler::Profiler::init();
+	PROFILE_FUNCTION
+	
+	for(int a = 0; a < 10000; ++a)
+	{
+		PROFILE_START("usleep(100)");
+		usleep(100);
+		PROFILE_END
+	}
+}
